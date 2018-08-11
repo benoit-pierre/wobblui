ee596e490f2c2ab9ce9c46a01adf8da85394533f
Jonas Thiem <jonas@thiem.email>
2018-08-12 01:25:44 +0200

    Fix focus issue

--- src/wobblui/window.py
+++ src/wobblui/window.py
@@ -219,13 +219,7 @@ class Window(WidgetBase):
     def add(self, *args, **kwargs):
         return_value = super().add(*args, **kwargs)
         if len(self._children) > 0:
-            focus_item = WidgetBase.\
-                get_focused_widget(self._children[0])
-            if focus_item is None:
-                for child in self._children:
-                    if child.focusable:
-                        self._children[0].focus()
-                        break
+            self.focus_update()
         return return_value
 
     def do_redraw(self):
