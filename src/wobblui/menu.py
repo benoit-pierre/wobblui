712c931c37bbdaf9fb777c2cc2fcaeaa0004f266
Jonas Thiem <jonas@thiem.email>
2018-08-17 13:05:08 +0200

    Add menu callback code

--- src/wobblui/menu.py
+++ src/wobblui/menu.py
@@ -5,6 +5,19 @@ from wobblui.list import ListBase
 class Menu(ListBase):
     def __init__(self):
         super().__init__(render_as_menu=True)
+        self.callback_funcs = []
+        self.triggered_by_single_click = True
+
+    def on_triggered(self):
+        item_id = self.selected_index
+        if item_id >= 0:
+            self.focus_next()
+            if item_id < len(self.callback_funcs):
+                self.callback_funcs[item_id]()
+
+    def add(self, text, func_callback=None):
+        super().add(text)
+        self.callback_funcs.append(func_callback)
 
     def on_keydown(self, virtual_key, physical_key, modifiers):
         if virtual_key == "escape":
