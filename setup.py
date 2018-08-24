76c3ee71ec072a4dcccd0449c8a7772644153109
Jonas Thiem <jonas@thiem.email>
2018-08-24 15:50:00 +0200

    Add version info into importable module

--- setup.py
+++ setup.py
@@ -6,9 +6,20 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open("src/wobblui/version.py") as fh:
+    contents = fh.read()
+    for line in contents.replace("\r\n", "\n").split("\n"):
+        if line.replace(" ", "").startswith("VERSION="):
+            v = line.partition("=")[2].\
+                partition("#")[0].strip()
+            if (v.startswith("\"") and v.endswith("\"")) or \
+                    (v.startswith("'") or v.endswith("'")):
+                v = v[1:-1]
+            package_version = v
+
 setuptools.setup(
     name="wobblui",
-    version="0.0.1",
+    version=package_version,
     author="Jonas Thiem",
     author_email="jonas@thiem.email",
     description="A simple, universal and " +
