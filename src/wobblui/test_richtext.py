a0c4c9abbc1b65bd42302e885bd22e68dbf1d09a
Jonas Thiem <jonas@thiem.email>
2018-10-09 02:07:26 +0200

    Add richtext basic layouting test with perf output

---
+++ src/wobblui/test_richtext.py
@@ -0,0 +1,27 @@
+
+import sys
+import time
+
+from wobblui.richtext import RichText
+
+def test_perf(capsys):
+    try:
+        with capsys.disabled():  # ensure perf data is printed
+            i = 0
+            while i < 5:
+                obj = RichText()
+                test_text = ("Lorem ipsum - this is a " +
+                    "test text. It is very long.") * 50
+                t1 = time.monotonic()
+                obj.set_text(test_text)
+                t2 = time.monotonic()
+                obj.layout(max_width=100)
+                t3 = time.monotonic()
+                print("Times: setting text: " +
+                    str(round((t2 - t1) * 1000.0)) + "ms" +
+                    ", layouting: " +
+                    str(round((t3 - t2) * 1000.0)) + "ms",
+                    file=sys.stderr, flush=True)
+                i += 1
+    except Exception as e:
+        raise e
