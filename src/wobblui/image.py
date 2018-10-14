22a33ac6affda72d3bad625b42285bb3453472c7
Jonas Thiem <jonas@thiem.email>
2018-10-14 10:09:04 +0200

    Add image widget for displaying images

--- src/wobblui/image.py
+++ src/wobblui/image.py
@@ -22,9 +22,11 @@ freely, subject to the following restrictions:
 import ctypes
 import io
 import os
+import PIL.Image
 import sdl2 as sdl
 import sdl2.sdlimage as sdlimage
 
+from wobblui.osinfo import is_android
 from wobblui.sdlinit import initialize_sdl
 from wobblui.widget import Widget
 
@@ -110,4 +112,80 @@ def remove_image_alpha(pil_image):
     else:
         raise RuntimeError("unsupported mode: " +
             pil_Image.mode)
+
+class ImageWidget(Widget):
+    def __init__(self, pil_image,
+            fit_to_width=None,
+            fit_to_height=None):
+        super().__init__()
+        if type(pil_image) == str or type(pil_image) == bytes:
+            pil_image = PIL.Image.open(pil_image)
+        self.pil_image = pil_image.copy()
+        self.pil_image_small = pil_image
+        max_size = 4096
+        if is_android():
+            max_size = 1024
+        (imgw, imgh) = self.pil_image_small.size
+        if imgw > max_size or imgh > max_size:
+            scaledown_w = (max_size / imgw)
+            scaledown_h = (max_size / imgh)
+            scaledown = min(scaledown_w, scaledown_h)
+            self.pil_image_small = self.pil_image_small.resize(
+                [max(1, round(imgw * scaledown)),
+                max(1, round(imgh * scaledown))], PIL.Image.ANTIALIAS)
+        self.fit_to_width = fit_to_width
+        self.fit_to_height = fit_to_height
+        self.image_texture = None
+
+    def update_renderer(self):
+        if self.image_texture != None:
+            sdl.SDL_DestroyTexture(self.image_texture)
+            self.image_texture = None
+
+    def on_redraw(self):
+        if self.renderer is None:
+            return
+        if self.image_texture is None:
+            self.image_texture = image_to_sdl_texture(
+                self.renderer, self.pil_image_small)
+        tg = sdl.SDL_Rect()
+        tg.x = 0
+        tg.y = 0
+        (imgw, imgh) = self.pil_image.size
+        scale_w = (self.width / imgw)
+        scale_h = (self.height / imgh)
+        scale = min(scale_w, scale_h)
+        tg.w = round(imgw * scale)
+        tg.h = round(imgh * scale)
+        sdl.SDL_SetRenderDrawColor(self.renderer, 255, 255, 255, 255)
+        sdl.SDL_RenderCopy(self.renderer, self.image_texture,
+            None, tg)
+
+    def get_natural_width(self):
+        return self._natural_size()[0]
+
+    def get_natural_height(self, given_width=None):
+        (w, h) = self._natural_size()
+        if w > given_width:
+            reduce_f = (given_width / w)
+            return round(h * reduce_f)
+        return h
+
+    def _natural_size(self):
+        (w, h) = self.pil_image.size
+        w *= self.dpi_scale
+        h *= self.dpi_scale
+        if self.fit_to_width != None:
+            max_w = round(self.fit_to_width * self.dpi_scale)
+            if w > max_w:
+                reduce_f = (max_w / w)
+                h = round(h * reduce_f)
+                w = max_w
+        if self.fit_to_height != None:
+            max_h = round(self.fit_to_height * self.dpi_scale)
+            if h > max_h:
+                reduce_f = (max_h / h)
+                w = round(w * reduce_f)
+                h = max_h
+        return (w, h)
 
