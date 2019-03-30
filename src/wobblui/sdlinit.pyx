b9562887f02b3adc492fe0351372ab60749b0e90
Jonas Thiem <jonas@thiem.email>
2019-03-30 16:30:51 +0100

    Actually disable mouse/touch separation

--- src/wobblui/sdlinit.pyx
+++ src/wobblui/sdlinit.pyx
@@ -52,7 +52,7 @@ cpdef void initialize_sdl():
         sdl.SDL_HINT_OVERRIDE
     )
     sdl.SDL_SetHintWithPriority(
-        b"SDL_ANDROID_SEPARATE_MOUSE_AND_TOUCH", b"1",
+        b"SDL_ANDROID_SEPARATE_MOUSE_AND_TOUCH", b"0",
         sdl.SDL_HINT_OVERRIDE,
     )  # disable for now.
     sdl.SDL_SetHintWithPriority(
